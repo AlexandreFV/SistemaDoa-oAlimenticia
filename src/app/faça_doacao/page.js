f0857a3fe6642a1911cf88e1791ec1a97ba82fa4
washingtonmiscossi123@gmail.com <washintonmiscossi123@gmail.com>
2024-03-18 10:49:49 -0300

    fazendo_tela_doacao

---
+++ src/app/faça_doacao/page.js
@@ -0,0 +1,14 @@
+// pages/add-product.js
+import React from 'react';
+import ProductForm from '../components/ProductForm';
+
+const AddProductPage = () => {
+  return (
+    <div>
+      <h1>Cadastrar Produto</h1>
+      <ProductForm />
+    </div>
+  );
+};
+
+export default AddProductPage;
