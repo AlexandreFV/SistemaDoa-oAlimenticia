9083781cbb1592f28ca4dc659fd67725a6377b4e
ArthurAmericoDurante <arthurdurante.9@gmail.com>
2024-04-18 19:55:17 -0300

    Tela Contribuição financeira - Dados Financeiros Concluída.

--- src/app/ContribFinanceiraInserirDados/page.js
+++ src/app/ContribFinanceiraInserirDados/page.js
@@ -24,63 +24,65 @@ export default function ContribFinanceiraInserirDados() {
 
                         <BackButton />
 
-                        <h1 className="titulo">Faça sua doação</h1>
+                        <h1 className="titulo">Contribuição Financeira</h1>
                         <div className="DivFundoFilho">
+                            <div className="faixaEsquerda"></div>
+                            <div className="faixaDireita"></div>
+                            <div className="posfundoFilho" >
 
-
-                            <form>
-                                <div className="DadosFinanc" style={{ display: "flex", justifyContent: "center" }}>
-                                    <p style={{ marginTop: "1.8rem", fontSize: "1.2rem", marginBottom: "1.8rem" }}>Dados Fianceiros</p>
-                                </div>
-                                <div style={{ display: "flex", justifyContent: "center", gap: "8%" }}>
-                                    <div style={{ marginLeft: "10px" }}>
-                                        <label>
-                                            Nome do Produto:
-                                        </label>
-                                        <input type="text" name="nome_alimento" className="form-control"
-                                            placeholder="Digite o nome do produto..." /*value={nome_alimento}
-                                        onChange={(e) => setNome(e.target.value)}*/ />
+                                <form>
+                                    <div className="DadosFinanc" style={{ display: "flex", justifyContent: "center" }}>
+                                        <p style={{ marginTop: "1.8rem", fontSize: "1.2rem", marginBottom: "1.8rem", fontWeight: "bold" }}>Dados Fianceiros</p>
                                     </div>
-                                    <div>
-                                        <label className="categoriaLabel">
-                                            Categoria:
-                                        </label>
-
-                                        <div class="form-group">
-                                            <select name="categoria" className="form-control catego" value={categoria} onChange={(e) => setCategoria(e.target.value)}>
-                                                <option value="">Selecione a categoria</option>
-                                                <option value="fruta">Pix</option>
-                                                <option value="tuberculo">Transferência Bancária</option>
-                                                <option value="hortalicia">Cartão de Crédito</option>
-                                                <option value="marmita">Cartão de Débito</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="DadosFinanc" style={{ display: "flex", justifyContent: "center" }}>
-                                    <p style={{ marginTop: "1.8rem", fontSize: "1.2rem", marginBottom: "1.8rem" }}>Valor de Envio</p>
-                                </div>
-                                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "8%" }}>
-                                    <div style={{ marginLeft: "10px" }}>
-                                        <p>Nome do Intermediário</p>
-                                        <p>-Nome do Intermediário-</p>
-                                    </div>
-                                    <div>
-                                        <div style={{ marginLeft: "10px" }}>
+                                    <div style={{ display: "flex", justifyContent: "center", gap: "8%" }}>
+                                        <div style={{}}>
                                             <label>
-                                                Digite o valor de envio:
+                                                Nome da Empresa:
                                             </label>
                                             <input type="text" name="nome_alimento" className="form-control"
-                                                placeholder="Digite o valor..." /*value={nome_alimento}
+                                                placeholder="Digite o nome do produto..." /*value={nome_alimento}
                                         onChange={(e) => setNome(e.target.value)}*/ />
                                         </div>
+                                        <div>
+                                            <label className="categoriaLabel">
+                                                Categoria:
+                                            </label>
+
+                                            <div class="form-control">
+                                                <select name="categoria" className="catego" value={categoria} onChange={(e) => setCategoria(e.target.value)}>
+                                                    <option value="">Selecione a categoria...</option>
+                                                    <option value="fruta">Pix</option>
+                                                    <option value="tuberculo">Transferência Bancária</option>
+                                                    <option value="hortalicia">Cartão de Crédito</option>
+                                                    <option value="marmita">Cartão de Débito</option>
+                                                </select>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                    <div className="DadosFinanc" style={{ display: "flex", justifyContent: "center" }}>
+                                        <p style={{ marginTop: "1.8rem", fontSize: "1.2rem", marginBottom: "1.8rem", fontWeight: "bold" }}>Valor de Envio</p>
+                                    </div>
+                                    <div className="valorEnvio">
+                                        <div style={{ marginLeft: "5%" }}>
+                                            <p>Nome do Intermediário</p>
+                                            <p>-Nome do Intermediário-</p>
+                                        </div>
+                                        <div className="campoValor">
+                                            <div className="poscampoValor" style={{}}>
+                                                <label>
+                                                    Digite o valor de envio:
+                                                </label>
+                                                <input type="text" name="nome_alimento" className="form-control"
+                                                    placeholder="Digite o valor..." /*value={nome_alimento}
+                                        onChange={(e) => setNome(e.target.value)}*/ />
+                                            </div>
+                                        </div>
+                                    </div>
 
-                                <button className="btnEnvi" type='submit'>Enviar</button>
+                                    <button className="btnEnvi" type='submit'>Enviar</button>
 
-                            </form>
-
+                                </form>
+                            </div>
 
                         </div>
 
