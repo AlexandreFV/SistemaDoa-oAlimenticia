65204394ee231e32223f51f3db733d3eff31cc42
Eduardo Belardinuci <belardinucieduardo@gmail.com>
2024-05-08 20:55:27 -0300

    Att
    Melhoria tela ListBenefIntermed

--- src/app/ListBenefIntermed/page.js
+++ src/app/ListBenefIntermed/page.js
@@ -22,17 +22,17 @@ export default function ListProdutorIntermed() {
     const [produtosEnviados, setProdEnviados] = useState(null);
     useEffect(() => {
         const token = localStorage.getItem("token");
-         
-        if(!token){
+
+        if (!token) {
 
             router.push("/Cadastrar");
-        }else {
+        } else {
 
-        const userType = localStorage.getItem("userType");
-        if(userType != "intermediario"){
-            router.push("/PermissaoNegadaIntermediario");
+            const userType = localStorage.getItem("userType");
+            if (userType != "intermediario") {
+                router.push("/PermissaoNegadaIntermediario");
+            }
         }
-        } 
     })
 
     useEffect(() => {
@@ -45,17 +45,17 @@ export default function ListProdutorIntermed() {
                     method: "GET",
                     headers: { 'Authorization': `Bearer ${token}` }
                 });
-    
+
                 if (response.ok) {
                     const data = await response.json();
                     setProdutosComprados(data.produtosComprados);
                     const prodComp = data.produtosComprados;
 
-                    const response2 = await fetch (`http://localhost:3001/MeusProdutosDistribuidos/${usuarioIntermediarioId}`, {
+                    const response2 = await fetch(`http://localhost:3001/MeusProdutosDistribuidos/${usuarioIntermediarioId}`, {
                         method: "GET",
                         headers: { 'Authorization': `Bearer ${token}` }
                     });
-        
+
                     if (response2.ok) {
                         const data2 = await response2.json();
                         const produtosDistribuidos = data2.produtosDistribuidos;
@@ -64,10 +64,10 @@ export default function ListProdutorIntermed() {
                         const produtosEnviadosIds = prodComp.map(prod => prod.id);
                         const produtosEnviados = produtosDistribuidos.filter(prodDist => produtosEnviadosIds.includes(prodDist.produtoCompradoId));
 
-                    // Atualizar o estado com os produtos enviados
-                    console.log("Ids iguais: ",produtosEnviados);
-                    setProdEnviados(produtosEnviados);
-                }
+                        // Atualizar o estado com os produtos enviados
+                        console.log("Ids iguais: ", produtosEnviados);
+                        setProdEnviados(produtosEnviados);
+                    }
                 }
             } catch (error) {
                 console.log(error);
@@ -75,9 +75,9 @@ export default function ListProdutorIntermed() {
         };
         handleExibir();
     }, []);
-    
 
-    const handleExibirBenef = async (idUser, idProd,nomeProduto) => {
+
+    const handleExibirBenef = async (idUser, idProd, nomeProduto) => {
         try {
             const token = localStorage.getItem('token');
             const response = await fetch(`http://localhost:3001/MeusIntermedios/${idUser}/${idProd}`, {
@@ -87,12 +87,12 @@ export default function ListProdutorIntermed() {
 
             if (response.ok) {
                 const data = await response.json();
-              
-                    setIdProd(idProd);  
-                    setIdUser(idUser);
-                    setNomeProd(nomeProduto);
-                    // router.push('/ProdutosBenef');
-                
+
+                setIdProd(idProd);
+                setIdUser(idUser);
+                setNomeProd(nomeProduto);
+                // router.push('/ProdutosBenef');
+
 
             } else {
                 console.log("Tws");
@@ -104,69 +104,73 @@ export default function ListProdutorIntermed() {
 
     return (
         <>
-        {idProd ? (
-          <ProdutosDistribuidos idUser={idUser} idProd={idProd} nomeProduto={nomeProduto} />
-        ) : (
-        <div className="DPLISTBENE">
-            <Navbar></Navbar>
-            <div className="DFINFOPROD">
-                <MenuDireito />
-                <div className="DIINFOPROD">
-                    <div className="DFPINFOPROD">
-                        <BackButton />
-                        <h1 className="h1MinDoaINFOPROD">Registro de Compras</h1>
-                        <div className="DFFINFOPROD">
-                            <div style={{ display: "flex", alignItens: "center", textAlign: "center" }}>
-                                <h1 style={{ display: "flex", justifyContent: "center", alignItems: "center", left: "1.6rem" }} className="h1ProdutosEnvINFOPROD">Lista de Produtos Comprados</h1>
-                                <div style={{ marginRight: "1.2rem", marginTop: "1rem" }}>
-                                    <img src="./filtrar.png" className="imgFiltrarINFOPROD"></img>
-                                    <p >Filtro</p>
+            {idProd ? (
+                <ProdutosDistribuidos idUser={idUser} idProd={idProd} nomeProduto={nomeProduto} />
+            ) : (
+                <div className="DPLISTBENE">
+                    <Navbar></Navbar>
+                    <div className="DFINFOPROD">
+                        <MenuDireito />
+                        <div className="DIINFOPROD">
+                            <div className="DFPINFOPROD">
+                                <BackButton />
+                                <h1 className="h1MinDoaINFOPROD">Registro de Compras</h1>
+                                <div className="DFFINFOPROD">
+                                    <div style={{ display: "flex", alignItens: "center", textAlign: "center" }}>
+                                        <h1 style={{ display: "flex", justifyContent: "center", alignItems: "center", left: "1.6rem" }} className="h1ProdutosEnvINFOPROD">Lista de Produtos Comprados</h1>
+                                        <div style={{ marginRight: "1.2rem", marginTop: "1rem" }}>
+                                            <img src="./filtrar.png" className="imgFiltrarINFOPROD"></img>
+                                            <p >Filtro</p>
+                                        </div>
+                                    </div>
+                                    <div style={{ backgroundColor: "black", width: "100%", height: "2px" }}></div>
+                                    {produtosComprados.length === 0 ? (
+                                        <div>
+                                            <img src="/triste.png" className="LISTBENEFINTERIMG"></img>
+                                            <h1 className="LISTBENEFINTERTXT1">Não temos produtos disponíveis no momento.</h1>
+                                            <p className="LISTBENEFINTERTXT2">Aguarde até que tenhamos um novo produto.</p>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            {produtosComprados.map((meusprodutos, index) => (
+
+                                                <div key={index}>
+                                                    <div className="CardProduct" style={{ width: "90%", height: "9rem", background: "#EBEBEB", borderRadius: "10px", marginTop: "2rem", marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center" }}>
+
+                                                        <div style={{ float: "left", flexWrap: "wrap", marginLeft: "0", marginRight: "0" }}>
+                                                            <div style={{ marginLeft: "2rem", marginTop: "1rem", fontSize: 18, fontFamily: "Inter", fontWeight: "bold" }}>{meusprodutos.usuariodoador.nome} </div>
+                                                            <div style={{ marginLeft: "2rem", marginTop: "1.2rem", fontSize: "1rem", fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word', display: "flex", alignItems: "center" }}>
+                                                                <img src="/icon_document.png" style={{ marginRight: "0.1rem", width: "1.3rem" }}></img>CNPJ/CPF: {meusprodutos.usuariodoador.cpf}
+                                                            </div>
+                                                            <div style={{ marginLeft: "2rem", marginTop: "0.5rem", fontSize: "1rem", fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word', display: "flex", alignItems: "center" }}>
+                                                                <img src="/icon_tel.png" style={{ marginRight: "0.32rem", width: "1.1rem" }}></img>Contato:{meusprodutos.usuariodoador.telefone}
+                                                            </div>
+                                                        </div>
+                                                        <div className="linha_vertical" style={{ backgroundColor: "black", height: "100%", width: "2px", marginLeft: "auto", marginRight: "0" }}></div>
+                                                        <div style={{ flexWrap: "wrap", marginLeft: "2rem", marginRight: "auto", marginTop: "1rem" }}>
+                                                            <p style={{ fontFamily: "Inter" }}>Produto comprado: {meusprodutos.nome_alimento}</p>
+                                                            <p style={{ fontFamily: "Inter", marginTop: "0.4rem" }}>Categoria: {meusprodutos.categoria}</p>
+                                                            <p style={{ fontFamily: "Inter", marginTop: "0.4rem" }}>Formato: Caixa</p>
+                                                            <p style={{ fontFamily: "Inter", marginTop: "0.4rem" }}>Quantidade: {meusprodutos.quantidade}</p>
+                                                        </div>
+                                                        <button style={{ marginRight: "10px", padding: "0px", opacity: produtosEnviados && produtosEnviados.some(prod => prod.produtoCompradoId === meusprodutos.id) ? "1" : "0.5", pointerEvents: produtosEnviados && produtosEnviados.some(prod => prod.produtoCompradoId === meusprodutos.id) ? "auto" : "none" }} onClick={() => handleExibirBenef(meusprodutos.usuariointermediarioId, meusprodutos.id, meusprodutos.nome_alimento)}>
+                                                            <span>Envios</span>
+                                                            <img src="./car.png" className="carImg" alt="Car"></img>
+
+                                                        </button>
+
+                                                    </div>
+                                                    <div style={{ fontSize: 16, fontFamily: 'Inter', fontWeight: '500', textAlign: "right", marginRight: "5%" }}><b>Data de Compra:  {meusprodutos.createdAt && new Date(meusprodutos.createdAt).toLocaleDateString()}</b></div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
                                 </div>
                             </div>
-                            <div style={{ backgroundColor: "black", width: "100%", height: "2px" }}></div>
-                            {produtosComprados.length === 0 ? (
-                                <p>Não há produtos Disponiveis</p>
-                            ) : (
-                                <div>
-                                    {produtosComprados.map((meusprodutos, index) => (
-                                        
-                                        <div key={index}>
-                                        <div className="CardProduct" style={{ width: "90%", height: "9rem", background: "#EBEBEB", borderRadius: "10px", marginTop: "2rem", marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}>
-
-                                                <div style={{ float: "left", flexWrap: "wrap", marginLeft: "0", marginRight: "0" }}>
-                                                    <div style={{ marginLeft: "2rem", marginTop: "1rem", fontSize: 18, fontFamily: "Inter", fontWeight: "bold" }}>{meusprodutos.usuariodoador.nome} </div>
-                                                    <div style={{ marginLeft: "2rem", marginTop: "1.2rem", fontSize: "1rem", fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word', display: "flex", alignItems: "center" }}>
-                                                        <img src="/icon_document.png" style={{ marginRight: "0.1rem", width: "1.3rem" }}></img>CNPJ/CPF: {meusprodutos.usuariodoador.cpf}
-                                                    </div>
-                                                    <div style={{ marginLeft: "2rem", marginTop: "0.5rem", fontSize: "1rem", fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word', display: "flex", alignItems: "center" }}>
-                                                        <img src="/icon_tel.png" style={{ marginRight: "0.32rem", width: "1.1rem" }}></img>Contato:{meusprodutos.usuariodoador.telefone}
-                                                    </div>
-                                                </div>
-                                                <div className="linha_vertical" style={{ backgroundColor: "black", height: "100%", width: "2px", marginLeft: "auto", marginRight: "0" }}></div>
-                                                <div style={{ flexWrap: "wrap", marginLeft: "2rem", marginRight: "auto", marginTop: "1rem" }}>
-                                                    <p style={{ fontFamily: "Inter" }}>Produto comprado: {meusprodutos.nome_alimento}</p>
-                                                    <p style={{ fontFamily: "Inter", marginTop: "0.4rem" }}>Categoria: {meusprodutos.categoria}</p>
-                                                    <p style={{ fontFamily: "Inter", marginTop: "0.4rem" }}>Formato: Caixa</p>
-                                                    <p style={{ fontFamily: "Inter", marginTop: "0.4rem" }}>Quantidade: {meusprodutos.quantidade}</p>
-                                                </div>
-                                                <button style={{marginRight:"10px",padding:"0px",opacity: produtosEnviados && produtosEnviados.some(prod => prod.produtoCompradoId === meusprodutos.id) ? "1" : "0.5", pointerEvents: produtosEnviados && produtosEnviados.some(prod => prod.produtoCompradoId === meusprodutos.id) ? "auto" : "none" }} onClick={() => handleExibirBenef(meusprodutos.usuariointermediarioId, meusprodutos.id, meusprodutos.nome_alimento)}>
-                                                <span>Envios</span>
-                                                <img src="./car.png" className="carImg" alt="Car"></img>
-
-                                                </button>
-
-                                            </div>
-                                            <div style={{ fontSize: 16, fontFamily: 'Inter', fontWeight: '500', textAlign: "right", marginRight: "5%" }}><b>Data de Compra:  {meusprodutos.createdAt && new Date(meusprodutos.createdAt).toLocaleDateString()}</b></div>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    )}
-    </>
+            )}
+        </>
     );
 }
